2c04e2f104943e934dda52c57f9b95e581e7b5ed
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2025-11-29 01:30:37 +0400

    Uncomment code for user preferences file handling

--- src/user_preferences.cpp
+++ src/user_preferences.cpp
@@ -212,17 +212,17 @@ void CUserPreferencesSystem::SetPreference(int iSlot, const char* sKey, const ch
 
 	ZEPlayer* player = g_playerManager->GetPlayer(CPlayerSlot(iSlot));
 
-	// char szPath[MAX_PATH];
-	// V_snprintf(szPath, sizeof(szPath), "%s%s", Plat_GetGameDirectory(), "/csgo/addons/cs2fixes/data/user_preferences/user_preferences.ini");
+	char szPath[MAX_PATH];
+	V_snprintf(szPath, sizeof(szPath), "%s%s", Plat_GetGameDirectory(), "/csgo/addons/cs2fixes/data/user_preferences/user_preferences.ini");
 
-	// KeyValues *hData = g_hKVData->FindKey(std::to_string(player->GetSteamId64()).c_str(), true);
- //    if (hData)
-	// {
-	// 	hData->SetString(sKey, sValue);
+	KeyValues *hData = g_hKVData->FindKey(std::to_string(player->GetSteamId64()).c_str(), true);
+    if (hData)
+	{
+		// hData->SetString(sKey, sValue);
 
-	// 	if (!hData->SaveToFile(g_pFullFileSystem, szPath))
-	// 		Warning("Failed to save infractions to %s\n", szPath);
-	// }
+		if (!hData->SaveToFile(g_pFullFileSystem, szPath))
+			Warning("Failed to save infractions to %s\n", szPath);
+	}
 }
 
 void CUserPreferencesSystem::SetPreferenceInt(int iSlot, const char* sKey, int iValue)
