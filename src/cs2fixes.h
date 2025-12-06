04c3506cf890ca355e877da9f15d00c7d7fc1605
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2025-12-06 04:00:10 +0400

    Update cs2fixes.h

--- src/cs2fixes.h
+++ src/cs2fixes.h
@@ -1,22 +1,3 @@
-﻿/**
- * =============================================================================
- * CS2Fixes
- * Copyright (C) 2023-2025 Source2ZE
- * =============================================================================
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, version 3.0, as published by the
- * Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #include "engine/igameeventsystem.h"
@@ -38,12 +19,14 @@
 	#include "version_gen_placeholder.h"
 #endif
 
+// Forward declarations
 class CCSPlayer_MovementServices;
 class CServerSideClient;
 struct TouchLinked_t;
 class CCSPlayer_WeaponServices;
 class CBasePlayerWeapon;
 
+// External variables
 extern IUtilsApi* g_pUtils;
 extern IPlayersApi* g_pPlayers;
 extern IMenusApi* g_pMenus;
@@ -62,89 +45,94 @@ extern CServerSideClient* GetClientBySlot(CPlayerSlot slot);
 extern void FullUpdateAllClients();
 extern CConVar<bool> g_cvarDropMapWeapons;
 
+// Define the callback type (function pointer)
+typedef void (*CS2FixesLoadedCallback)(CCSPlayerPawn* pPawn);
+
 class CS2Fixes : public ISmmPlugin, public IMetamodListener, public ICS2Fixes
 {
 public:
-	bool Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late);
-	void Hook_PostEvent(CSplitScreenSlot nSlot, bool bLocalOnly, int nClientCount, const uint64* clients,
-						INetworkMessageInternal* pEvent, const CNetMessage* pData, unsigned long nSize, NetChannelBufType_t bufType);
-	bool Unload(char* error, size_t maxlen);
-	bool Pause(char* error, size_t maxlen);
-	bool Unpause(char* error, size_t maxlen);
-	void AllPluginsLoaded();
+    bool Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late);
+    void Hook_PostEvent(CSplitScreenSlot nSlot, bool bLocalOnly, int nClientCount, const uint64* clients,
+                        INetworkMessageInternal* pEvent, const CNetMessage* pData, unsigned long nSize, NetChannelBufType_t bufType);
+    bool Unload(char* error, size_t maxlen);
+    bool Pause(char* error, size_t maxlen);
+    bool Unpause(char* error, size_t maxlen);
+    void AllPluginsLoaded();
 
 public: // hooks
-	void Hook_GameServerSteamAPIActivated();
-	void Hook_GameServerSteamAPIDeactivated();
-	void OnLevelInit(char const* pMapName,
-					 char const* pMapEntities,
-					 char const* pOldLevel,
-					 char const* pLandmarkName,
-					 bool loadGame,
-					 bool background);
-	void OnLevelShutdown();
-	void Hook_GameFramePost(bool simulating, bool bFirstTick, bool bLastTick);
-	void Hook_ClientActive(CPlayerSlot slot, bool bLoadGame, const char* pszName, uint64 xuid);
-	void Hook_ClientDisconnect(CPlayerSlot slot, ENetworkDisconnectionReason reason, const char* pszName, uint64 xuid, const char* pszNetworkID);
-	void Hook_ClientPutInServer(CPlayerSlot slot, char const* pszName, int type, uint64 xuid);
-	void Hook_ClientSettingsChanged(CPlayerSlot slot);
-	void Hook_OnClientConnected(CPlayerSlot slot, const char* pszName, uint64 xuid, const char* pszNetworkID, const char* pszAddress, bool bFakePlayer);
-	bool Hook_ClientConnect(CPlayerSlot slot, const char* pszName, uint64 xuid, const char* pszNetworkID, bool unk1, CBufferString* pRejectReason);
-	void Hook_ClientCommand(CPlayerSlot nSlot, const CCommand& _cmd);
-	void Hook_CheckTransmit(CCheckTransmitInfo** ppInfoList, int infoCount, CBitVec<16384>& unionTransmitEdicts,
-							CBitVec<16384>&, const Entity2Networkable_t** pNetworkables, const uint16* pEntityIndicies, int nEntities);
-	void Hook_DispatchConCommand(ConCommandRef cmd, const CCommandContext& ctx, const CCommand& args);
-	void Hook_CGamePlayerEquipUse(class InputData_t*);
-	void Hook_CGamePlayerEquipPrecache(CEntityPrecacheContext*);
-	void Hook_CTriggerGravityPrecache(CEntityPrecacheContext* param);
-	void Hook_CTriggerGravityEndTouch(CBaseEntity* pOther);
-	void Hook_StartupServer(const GameSessionConfiguration_t& config, ISource2WorldSession*, const char*);
-	void Hook_ApplyGameSettings(KeyValues* pKV);
-	void Hook_CreateWorkshopMapGroup(const char* name, const CUtlStringList& mapList);
-	void Hook_GoToIntermission(bool bAbortedMatch);
-	bool Hook_OnTakeDamage_Alive(CTakeDamageResult* pDamageResult);
-	void Hook_PhysicsTouchShuffle(CUtlVector<TouchLinked_t>* pList, bool unknown);
+    void Hook_GameServerSteamAPIActivated();
+    void Hook_GameServerSteamAPIDeactivated();
+    void OnLevelInit(char const* pMapName,
+                     char const* pMapEntities,
+                     char const* pOldLevel,
+                     char const* pLandmarkName,
+                     bool loadGame,
+                     bool background);
+    void OnLevelShutdown();
+    void Hook_GameFramePost(bool simulating, bool bFirstTick, bool bLastTick);
+    void Hook_ClientActive(CPlayerSlot slot, bool bLoadGame, const char* pszName, uint64 xuid);
+    void Hook_ClientDisconnect(CPlayerSlot slot, ENetworkDisconnectionReason reason, const char* pszName, uint64 xuid, const char* pszNetworkID);
+    void Hook_ClientPutInServer(CPlayerSlot slot, char const* pszName, int type, uint64 xuid);
+    void Hook_ClientSettingsChanged(CPlayerSlot slot);
+    void Hook_OnClientConnected(CPlayerSlot slot, const char* pszName, uint64 xuid, const char* pszNetworkID, const char* pszAddress, bool bFakePlayer);
+    bool Hook_ClientConnect(CPlayerSlot slot, const char* pszName, uint64 xuid, const char* pszNetworkID, bool unk1, CBufferString* pRejectReason);
+    void Hook_ClientCommand(CPlayerSlot nSlot, const CCommand& _cmd);
+    void Hook_CheckTransmit(CCheckTransmitInfo** ppInfoList, int infoCount, CBitVec<16384>& unionTransmitEdicts,
+                            CBitVec<16384>&, const Entity2Networkable_t** pNetworkables, const uint16* pEntityIndicies, int nEntities);
+    void Hook_DispatchConCommand(ConCommandRef cmd, const CCommandContext& ctx, const CCommand& args);
+    void Hook_CGamePlayerEquipUse(class InputData_t*);
+    void Hook_CGamePlayerEquipPrecache(CEntityPrecacheContext*);
+    void Hook_CTriggerGravityPrecache(CEntityPrecacheContext* param);
+    void Hook_CTriggerGravityEndTouch(CBaseEntity* pOther);
+    void Hook_StartupServer(const GameSessionConfiguration_t& config, ISource2WorldSession*, const char*);
+    void Hook_ApplyGameSettings(KeyValues* pKV);
+    void Hook_CreateWorkshopMapGroup(const char* name, const CUtlStringList& mapList);
+    void Hook_GoToIntermission(bool bAbortedMatch);
+    bool Hook_OnTakeDamage_Alive(CTakeDamageResult* pDamageResult);
+    void Hook_PhysicsTouchShuffle(CUtlVector<TouchLinked_t>* pList, bool unknown);
 #ifdef PLATFORM_WINDOWS
-	Vector* Hook_GetEyePosition(Vector*);
-	QAngle* Hook_GetEyeAngles(QAngle*);
+    Vector* Hook_GetEyePosition(Vector*);
+    QAngle* Hook_GetEyeAngles(QAngle*);
 #else
-	Vector Hook_GetEyePosition();
-	QAngle Hook_GetEyeAngles();
+    Vector Hook_GetEyePosition();
+    QAngle Hook_GetEyeAngles();
 #endif
-	void Hook_CheckMovingGround(double frametime);
-	void Hook_DropWeaponPost(CBasePlayerWeapon* pWeapon, Vector* pVecTarget, Vector* pVelocity);
-	int Hook_LoadEventsFromFile(const char* filename, bool bSearchAll);
-	void Hook_SetGameSpawnGroupMgr(IGameSpawnGroupMgr* pSpawnGroupMgr);
+    void Hook_CheckMovingGround(double frametime);
+    void Hook_DropWeaponPost(CBasePlayerWeapon* pWeapon, Vector* pVecTarget, Vector* pVelocity);
+    int Hook_LoadEventsFromFile(const char* filename, bool bSearchAll);
+    void Hook_SetGameSpawnGroupMgr(IGameSpawnGroupMgr* pSpawnGroupMgr);
 
 private:
-	std::map<int, CS2FixesLoadedCallback> m_CS2FixesHook;
-public: // MetaMod API
-	void* OnMetamodQuery(const char* iface, int* ret);
-	std::uint64_t GetAdminFlags(std::uint64_t iSteam64ID) const override;
-	bool SetAdminFlags(std::uint64_t iSteam64ID, std::uint64_t iFlags) override;
-	int GetAdminImmunity(std::uint64_t iSteam64ID) const override;
-	bool SetAdminImmunity(std::uint64_t iSteam64ID, std::uint32_t iImmunity) override;
-	void HookCS2FixesLoaded(SourceMM::PluginId id, CS2FixesLoadedCallback callback) {
-		m_CS2FixesHook[id] = callback;
-	}
+    std::map<int, CS2FixesLoadedCallback> m_CS2FixesHook;
 
-	void CallApplyBaseClassVisuals(CCSPlayerPawn* pPawn) {
-		for (auto& callback : m_CS2FixesHook) {
-			if (callback.second) {
-				callback.second(pPawn);
-			}
-		}
-	}
+public: // MetaMod API
+    void* OnMetamodQuery(const char* iface, int* ret);
+    std::uint64_t GetAdminFlags(std::uint64_t iSteam64ID) const override;
+    bool SetAdminFlags(std::uint64_t iSteam64ID, std::uint64_t iFlags) override;
+    int GetAdminImmunity(std::uint64_t iSteam64ID) const override;
+    bool SetAdminImmunity(std::uint64_t iSteam64ID, std::uint32_t iImmunity) override;
+
+    void HookCS2FixesLoaded(SourceMM::PluginId id, CS2FixesLoadedCallback callback) {
+        m_CS2FixesHook[id] = callback;
+    }
+
+    void CallApplyBaseClassVisuals(CCSPlayerPawn* pPawn) {
+        for (auto& callback : m_CS2FixesHook) {
+            if (callback.second) {
+                callback.second(pPawn);
+            }
+        }
+    }
 
 public:
-	const char* GetAuthor() { return PLUGIN_AUTHOR; }
-	const char* GetName() { return PLUGIN_DISPLAY_NAME; }
-	const char* GetDescription() { return PLUGIN_DESCRIPTION; }
-	const char* GetURL() { return PLUGIN_URL; }
-	const char* GetLicense() { return PLUGIN_LICENSE; }
-	const char* GetVersion() { return PLUGIN_FULL_VERSION; }
-	const char* GetDate() { return __DATE__; }
-	const char* GetLogTag() { return PLUGIN_LOGTAG; }
+    const char* GetAuthor() { return PLUGIN_AUTHOR; }
+    const char* GetName() { return PLUGIN_DISPLAY_NAME; }
+    const char* GetDescription() { return PLUGIN_DESCRIPTION; }
+    const char* GetURL() { return PLUGIN_URL; }
+    const char* GetLicense() { return PLUGIN_LICENSE; }
+    const char* GetVersion() { return PLUGIN_FULL_VERSION; }
+    const char* GetDate() { return __DATE__; }
+    const char* GetLogTag() { return PLUGIN_LOGTAG; }
 };
 
 extern CS2Fixes g_CS2Fixes;
