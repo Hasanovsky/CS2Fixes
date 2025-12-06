fdffdd6c5c46e0935ec5f6ed82c0878ea07dab46
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2025-12-06 06:17:20 +0400

    Update ics2fixes.h

--- public/ics2fixes.h
+++ public/ics2fixes.h
@@ -23,7 +23,7 @@
 
 #define CS2FIXES_INTERFACE "CS2Fixes001"
 
-// typedef std::function<void(CCSPlayerPawn* pPawn)> HookCS2FixesLoaded;
+typedef std::function<void(CCSPlayerPawn* pPawn)> HookCS2FixesLoaded;
 
 class ICS2Fixes
 {
@@ -53,5 +53,5 @@ public:
 	virtual bool SetAdminImmunity(std::uint64_t iSteam64ID, std::uint32_t iImmunity) = 0;
 
 	// Hook ApplyBaseClassVisuals
-	// virtual void HookCS2FixesLoaded(SourceMM::PluginId id, CS2FixesLoadedCallback callback) = 0;
+	virtual void HookCS2FixesLoaded(SourceMM::PluginId id, HookCS2FixesLoaded callback) = 0;
 };
