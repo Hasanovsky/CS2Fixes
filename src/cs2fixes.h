c400e98b472c38f8f186f84e0ae6e955e28aeb79
Hasanovsky <58742759+Hasanovsky@users.noreply.github.com>
2025-12-06 06:16:49 +0400

    Update cs2fixes.h

--- src/cs2fixes.h
+++ src/cs2fixes.h
@@ -126,7 +126,7 @@ public: // MetaMod API
 	bool SetAdminFlags(std::uint64_t iSteam64ID, std::uint64_t iFlags) override;
 	int GetAdminImmunity(std::uint64_t iSteam64ID) const override;
 	bool SetAdminImmunity(std::uint64_t iSteam64ID, std::uint32_t iImmunity) override;
-	void HookCS2FixesLoaded(SourceMM::PluginId id, CS2FixesLoadedCallback callback) {
+	void HookCS2FixesLoaded(SourceMM::PluginId id, HookCS2FixesLoaded callback) {
 		mHookCS2FixesLoaded[id] = callback;
 	}
 	void CallApplyBaseClassVisuals(CCSPlayerPawn* pPawn) {
